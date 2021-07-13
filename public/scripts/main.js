14d6bf27da70e109b18704f7b267ff4f7e0ae9d9
Millena Gená <millenagena@gmail.com>
2021-07-13 20:44:43 -0300

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,11 +22,19 @@ deleteButton.forEach(button => {
 })
 
 function handleClick(event, check = true){
+    event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
     modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
     // abrir modal
     modal.open()
 }
